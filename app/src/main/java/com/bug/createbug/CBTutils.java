d398ef51eee13f0aafe73eea412e80682ef0e549
fengdd <Fengdongdong@2018>
2018-08-10 16:47:48 +0800

    sss

---
+++ app/src/main/java/com/bug/createbug/CBTutils.java
@@ -0,0 +1,13 @@
+package com.bug.createbug;
+
+/**
+ * on 2018/8/10.
+ */
+public class CBTutils {
+    public static void init(){
+        String abc = null;
+        if(abc.equals("1")){
+            //噜噜噜
+        }
+    }
+}
